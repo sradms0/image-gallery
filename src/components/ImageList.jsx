bc49dd5561dc196ab9b5b7a821e2d994c8192e0c
sradms0 <sradms0@openmailbox.org>
2019-10-14 17:44:59 -0400

    NotFound component

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ImageItem from './ImageItem';
+import NotFound from './NotFound';
 
 export default props => {
   const imageItems = props.images.map(imageItem => 
@@ -9,7 +10,7 @@ export default props => {
 
   const display = () => {
     if (imageItems.length > 0) return (<ul>{imageItems}</ul>);
-    else return (<p>That search did not return any results, please try again</p>);
+    else return (<NotFound/>);
   }
 
   return (<div className='photo-container'>{display()}</div>);
---
+++ src/components/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <li class="not-found">
+    <h3>No Results Found</h3>
+    <p>You search did not return any results. Please try again.</p>
+  </li>
+);
