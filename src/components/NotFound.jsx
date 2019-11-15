3bc06accabb49a299ec353aa0c7de1c9bbcdd1a5
sradms0 <sradms0@openmailbox.org>
2019-11-15 15:57:51 -0500

    change 'class' to 'className'

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,7 +6,7 @@ import React from 'react';
  * @component
  */
 const NotFound = () => (
-  <li class="not-found">
+  <li className="not-found">
     <h3>No Results Found</h3>
     <p>You search did not return any results. Please try again.</p>
   </li>
